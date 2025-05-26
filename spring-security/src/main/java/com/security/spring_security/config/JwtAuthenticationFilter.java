a260ba76a098723c7b3f4a26a896894c6c29bd0e
TkWarrior <talibulhaque369@gmail.com>
2025-05-26 15:28:22 +0530

    implemented extract username and token validity method in JwtService

--- spring-security/src/main/java/com/security/spring_security/config/JwtAuthenticationFilter.java
+++ spring-security/src/main/java/com/security/spring_security/config/JwtAuthenticationFilter.java
@@ -1,9 +1,17 @@
 package com.security.spring_security.config;
 
+import com.security.spring_security.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,9 +21,50 @@ import java.io.IOException;
 // OncePerRequestFilter - Filter comes in betweeen each ane every request
 // JwtAuthentication filter will be added before username and password filter
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
 
+        // if authorization header is null and it doesn't starts with Bearer means jwt token is not present then move to usernameand password filter
+        if(authHeader == null || !authHeader.startsWith("Bearer")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        final String jwt = authHeader.substring(7);
+        final String userName = jwtService.extractUserName(jwt);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(userName!=null && authentication == null){
+            //Authenticate
+
+            //getting userData from the database
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+
+            if(jwtService.isTokenValid(jwt,userDetails)){
+
+                // we are creating authentication token for the nextfilter
+               UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+               // setting the sessionID which is needed for the usernameandpassword authentication filter to validate the details from the security context
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource()
+                        .buildDetails(request));
+
+                SecurityContextHolder.getContext()
+                        .setAuthentication(authenticationToken);
+
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
--- spring-security/src/main/java/com/security/spring_security/service/JwtService.java
+++ spring-security/src/main/java/com/security/spring_security/service/JwtService.java
@@ -2,9 +2,11 @@ package com.security.spring_security.service;
 
 
 import com.security.spring_security.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -13,11 +15,14 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private String secretKey = null;
+
+    // this will generate the jwt token based on the generateKEY
     public String generateToken(User user) {
         Map<String,Object>claims = new HashMap<>();
         return Jwts
@@ -33,6 +38,7 @@ public class JwtService {
                 .compact();
     }
 
+    // This will genrate key based on the generate secretKey
     private SecretKey generateKey(){
         byte[] decode = Decoders.BASE64.decode(getSecretKey());
         return Keys.hmacShaKeyFor(decode);
@@ -41,5 +47,38 @@ public class JwtService {
     public String getSecretKey(){
         secretKey = "784d8bd6c3a7fa9bd7ea4a4bc99ab7686d8e17b9721f080cdca3340c6a28e8f19c93ff4566721482225fb2a651ab7f1dd0551560022a733a3c37260bc177f4d1";
         return secretKey;
+    }
+
+    public String extractUserName(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token , Function<Claims,T> claimResolver)
+    {
+       Claims claims = extractClaims(token);
+       return claimResolver.apply(claims);
+    }
+
+    private Claims extractClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(generateKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userName =  extractUserName(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    // if the time is before the cuurent time than the token is expired otherwise not expired
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        // extracting expiration time from the token payload
+        return extractClaims(token , Claims::getExpiration);
     }
 }
